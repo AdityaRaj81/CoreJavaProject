12c61114a6ef976b274ab094604ba875195ba6fa
Mr. Aditya <Connect2RajAditya@gmail.com>
2025-11-29 09:27:01 +0530

    add NeonNumber class to determine if a number is a neon number

---
+++ src/CoreJava/numbers/NeonNumber.java
@@ -0,0 +1,33 @@
+package CoreJava.numbers;
+
+import java.util.Scanner;
+
+public class NeonNumber {
+
+    public static boolean isNeonNumber(int num) {
+
+        int square = num * num;
+        int sum = 0;
+
+        while (square > 0) {
+            sum += square % 10;
+            square = square / 10;
+        }
+
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        if (isNeonNumber(num)) {
+            System.out.println(num + " is a neon number.");
+        } else {
+            System.out.println(num + " is not a neon number.");
+        }
+    }
+}
